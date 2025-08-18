8b0b529fcfd7a1b1af8204d6e0272e27fb15f95b
Cafe-GameDev <soarxz.games@gmail.com>
2025-08-17 22:15:54 -0300

    A

--- BodyLess/Scripts/UI/options_menu.gd
+++ BodyLess/Scripts/UI/options_menu.gd
@@ -1,1 +1,21 @@
 extends Control
+
+
+func _on_video_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_audio_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_language_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_apply_button_pressed() -> void:
+	pass # Replace with function body.
