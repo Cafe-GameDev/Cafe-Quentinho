0055a9da080a266e6251d515384e9445600fefcb
Cafe-GameDev <soarxz.games@gmail.com>
2025-08-26 00:09:30 -0300

    A

--- Essentials/Singletons/Scripts/debug_console.gd
+++ Essentials/Singletons/Scripts/debug_console.gd
@@ -18,7 +18,6 @@ func _process(_delta: float) -> void:
 	if visible:
 		_update_debug_info()
 
-
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_console"):
 		visible = not visible
